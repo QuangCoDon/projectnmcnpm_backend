a919f5efe092e065547bcb2529a2a00deaa47527
QuangCoDon <quangluuyenho5b@gmail.com>
2024-10-09 21:41:57 +0700

    FetchDateFromNewProduct

--- index.js
+++ index.js
@@ -125,6 +125,9 @@ app.post("/uploadProduct",async (req, res)=>{
     res.send({message: "Upload successfully" })
 })
 
-
+app.get("/product", async (req, res)=>{
+    const data = await productModel.find({})
+    res.send(JSON.stringify(data))
+})
 
 app.listen(PORT, ()=>console.log("Server is running at port : "+ PORT))
