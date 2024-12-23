27444f66b253c6853b341adabd231494675f788a
Nguyenvietcuongg <nguyenvietcuong12122004@gmail.com>
2024-12-23 20:27:15 +0700

    Discount

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: "10mb" }));
 app.use(
   express.urlencoded({
     extended: true,
-    limit: '10mb',
-  }),
+    limit: "10mb",
+  })
 );
 
 const PORT = process.env.PORT || 8080;
-//mongodb connection
-mongoose.set('strictQuery', false);
+
+// MongoDB Connection
+mongoose.set("strictQuery", false);
 mongoose
   .connect(process.env.MONGODB_URL)
-  .then(() => console.log('Connect to Database'))
-  .catch((err) => console.log(err));
+  .then(() => console.log("Connected to Database"))
+  .catch((err) => console.log("Error connecting to MongoDB:", err));
 
-//schema
+// Schemas
 const userSchema = mongoose.Schema({
   firstName: String,
   lastName: String,
@@ -33,53 +34,84 @@ const userSchema = mongoose.Schema({
   confirmPassword: String,
   image: String,
 });
+const userModel = mongoose.model("user", userSchema);
 
-//
-const userModel = mongoose.model('user', userSchema);
+const productSchema = mongoose.Schema({
+  name: String,
+  category: String,
+  image: String,
+  price: String,
+  description: String,
+});
+const productModel = mongoose.model("product", productSchema);
 
-//api
-app.get('/', (req, res) => {
-  res.send('Server is running');
+const contactSchema = mongoose.Schema({
+  name: String,
+  email: String,
+  phone: String,
+  message: String,
+  createdAt: { type: Date, default: Date.now },
+});
+const contactModel = mongoose.model("Contact", contactSchema);
+
+const discountSchema = mongoose.Schema({
+  code: { type: String, required: true },
+  type: { type: String, required: true },
+  value: { type: Number, required: true },
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
+
+  timeFrame: {
+    start: { type: String, required: true },
+    end: { type: String, required: true },
+  },
+  minimumOrderValue: { type: Number, required: true },
+  minimumItems: { type: Number, required: true },
+  applicableCategories: { type: [String], required: true },
+  usageLimit: { type: Number, required: true },
+});
+const discountModel = mongoose.model("Discount", discountSchema);
+
+// APIs
+app.get("/", (req, res) => {
+  res.send("Server is running");
 });
 
-
-//sign up
-app.post('/signup', async (req, res) => {
+// User APIs
+app.post("/signup", async (req, res) => {
   try {
     const { email } = req.body;
     if (!email) {
-      return res.status(400).send({ message: 'Email is required' });
+      return res.status(400).send({ message: "Email is required" });
     }
 
-    const result = await userModel.findOne({ email: email });
+    const result = await userModel.findOne({ email });
 
     if (result) {
       return res
         .status(400)
-        .send({ message: 'Email is already registered', alert: false });
+        .send({ message: "Email is already registered", alert: false });
     } else {
       const newUser = new userModel(req.body);
       await newUser.save();
       return res
         .status(200)
-        .send({ message: 'Successfully signed up', alert: true });
+        .send({ message: "Successfully signed up", alert: true });
     }
   } catch (err) {
     console.error(err);
-    return res.status(500).send({ message: 'An error occurred during signup' });
+    return res.status(500).send({ message: "An error occurred during signup" });
   }
 });
 
-//login api
-app.post('/login', async (req, res) => {
-  // console.log(req.body)
+app.post("/login", async (req, res) => {
   try {
     const { email } = req.body;
     if (!email) {
-      return res.status(400).send({ message: 'Email is required' });
+      return res.status(400).send({ message: "Email is required" });
     }
 
-    const result = await userModel.findOne({ email: email });
+    const result = await userModel.findOne({ email });
 
     if (result) {
       const dataSend = {
@@ -89,162 +121,163 @@ app.post('/login', async (req, res) => {
         email: result.email,
         image: result.image,
       };
-      console.log(dataSend);
-      return res
-        .status(200)
-        .send({
-          message: 'Login is successfully',
-          alert: true,
-          data: dataSend,
-        });
+      return res.status(200).send({
+        message: "Login is successfully",
+        alert: true,
+        data: dataSend,
+      });
     } else {
-      return res
-        .status(400)
-        .send({
-          message: 'Email is not available, please sign up',
-          alert: false,
-        });
+      return res.status(400).send({
+        message: "Email is not available, please sign up",
+        alert: false,
+      });
     }
   } catch (err) {
     console.error(err);
-    return res.status(500).send({ message: 'An error occurred during signup' });
+    return res.status(500).send({ message: "An error occurred during login" });
   }
 });
 
-//product section
-
-const schemaProduct = mongoose.Schema({
-  name: String,
-  category: String,
-  image: String,
-  price: String,
-  description: String,
-});
-const productModel = mongoose.model('product', schemaProduct);
-
-// save product in database
-// api
-app.post('/uploadProduct', async (req, res) => {
-  console.log(req.body);
-  const data = productModel(req.body);
-  const datasave = await data.save();
-  res.send({ message: 'Upload successfully' });
-});
-
-app.get('/product', async (req, res) => {
-  const data = await productModel.find({});
-  res.send(JSON.stringify(data));
-});
-app.get('/product/search', async (req, res) => {
+// Product APIs
+app.post("/uploadProduct", async (req, res) => {
   try {
-    const { name } = req.query;
+    const data = new productModel(req.body);
+    await data.save();
+    res.status(200).send({ message: "Product uploaded successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: "Failed to upload product" });
+  }
+});
 
-    if (!name || name.trim() === '') {
-      console.log('Search term missing');
-      return res.status(400).json({ error: 'Search term is required' });
+app.get("/product", async (req, res) => {
+  try {
+    const data = await productModel.find({});
+    res.status(200).json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: "Failed to fetch products" });
+  }
+});
+
+// Discount APIs
+app.post("/uploadDiscount", async (req, res) => {
+  try {
+    console.log("Received Discount Data:", req.body);
+
+    const {
+      code,
+      type,
+      value,
+      startDate,
+      endDate,
+
+      timeFrame,
+      minimumOrderValue,
+      minimumItems,
+      applicableCategories,
+      usageLimit,
+    } = req.body;
+
+    if (
+      !code ||
+      !type ||
+      !value ||
+      !startDate ||
+      !endDate ||
+      !timeFrame.start ||
+      !timeFrame.end ||
+      !minimumOrderValue ||
+      !minimumItems ||
+      applicableCategories.length === 0 ||
+      !usageLimit
+    ) {
+      return res.status(400).send({ message: "Missing required fields" });
     }
 
-    // Sử dụng regex để tìm kiếm sản phẩm theo tên
-    const query = { name: { $regex: name, $options: 'i'  } }; // options: i => không phân biệt chữ hoa, chữ thường
-    const data = await productModel.find(query); 
-    // console.log('Search Query:', query); // Log query để kiểm tra
-    // console.log('Search Results:', data); // Log kết quả từ database
-    res.status(200).json(data); 
-  } catch (error) {
-    console.error('Error searching for products:', error);
-    res.status(500).json({ error: 'Failed to search for products' });
+    const newDiscount = new discountModel(req.body);
+    await newDiscount.save();
+    res.status(200).send({ message: "Discount added successfully!" });
+  } catch (err) {
+    console.error("Error uploading discount:", err);
+    res.status(500).send({ message: "Failed to add discount" });
   }
 });
 
+app.get("/discounts", async (req, res) => {
+  try {
+    const discounts = await discountModel.find({});
+    res.status(200).json(discounts);
+  } catch (err) {
+    console.error("Error fetching discounts:", err);
+    res.status(500).send({ message: "Failed to fetch discounts" });
+  }
+});
 
+// Contact APIs
+app.post("/submit-contact", async (req, res) => {
+  try {
+    const { name, email, phone, message } = req.body;
 
-// payment api
-// mock payment endpoint
-app.post('/create-mock-checkout-session', async (req, res) => {
+    if (!name || !email || !phone || !message) {
+      return res.status(400).json({ message: "All fields are required." });
+    }
+
+    const newContact = new contactModel({ name, email, phone, message });
+    await newContact.save();
+    res.status(200).json({ message: "Form submitted successfully!" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error submitting the form." });
+  }
+});
+
+app.get("/get-contacts", async (req, res) => {
+  try {
+    const contacts = await contactModel.find().sort({ createdAt: -1 });
+    res.status(200).json(contacts);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error fetching contact data." });
+  }
+});
+
+// Payment APIs
+app.post("/create-mock-checkout-session", async (req, res) => {
   try {
     const items = req.body;
     if (!items || items.length === 0) {
       return res
         .status(400)
-        .json({ error: 'Cart is empty. Please add items to cart.' });
+        .json({ error: "Cart is empty. Please add items to cart." });
     }
     if (items.length < 2) {
-      return res
-        .status(400)
-        .json({
-          error: 'Insufficient items for checkout. Minimum 2 items required.',
-        });
+      return res.status(400).json({
+        error: "Insufficient items for checkout. Minimum 2 items required.",
+      });
     }
     const totalAmount = items.reduce(
       (acc, item) => acc + item.price * item.qty,
-      0,
+      0
     );
     if (totalAmount < 15) {
       return res
         .status(400)
-        .json({ error: 'Total amount is too low for checkout.' });
+        .json({ error: "Total amount is too low for checkout." });
     }
     const mockSession = {
-      sessionId: 'mock_session_id_123456',
-      message: 'This is a mock payment session',
-      paymentUrl: `${process.env.FRONTEND_URL}/success`, 
-      cancelUrl: `${process.env.FRONTEND_URL}/cancel`, 
+      sessionId: "mock_session_id_123456",
+      message: "This is a mock payment session",
+      paymentUrl: `${process.env.FRONTEND_URL}/success`,
+      cancelUrl: `${process.env.FRONTEND_URL}/cancel`,
     };
 
     res.status(200).json(mockSession);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'An error occurred in the mock payment' });
+    res.status(500).json({ message: "An error occurred in the mock payment" });
   }
 });
 
-
-const contactSchema = mongoose.Schema({
-  name: String,
-  email: String,
-  phone: String,
-  message: String,
-  createdAt: { type: Date, default: Date.now },
-});
-
-const contactModel = mongoose.model('Contact', contactSchema);
-
-// API để nhận dữ liệu form
-app.post('/submit-contact', async (req, res) => {
-  try {
-    const { name, email, phone, message } = req.body;
-
-    if (!name || !email || !phone || !message) {
-      return res.status(400).json({ message: 'All fields are required.' });
-    }
-
-    // Lưu vào MongoDB
-    const newContact = new contactModel({
-      name,
-      email,
-      phone,
-      message,
-    });
-
-    await newContact.save();
-
-    res.status(200).json({ message: 'Form submitted successfully!' });
-  } catch (error) {
-    console.error('Error saving contact form:', error);
-    res.status(500).json({ message: 'Error submitting the form.' });
-  }
-});
-
-// API để lấy danh sách đánh giá từ MongoDB
-app.get('/get-contacts', async (req, res) => {
-  try {
-    const contacts = await contactModel.find().sort({ createdAt: -1 }); // Sắp xếp theo ngày tạo (mới nhất trước)
-    res.status(200).json(contacts);
-  } catch (error) {
-    console.error('Error fetching contacts:', error);
-    res.status(500).json({ message: 'Error fetching contact data.' });
-  }
-});
-
-//Gửi kết nối messages phản hồi với database
-app.listen(PORT, () => console.log('Server is running at port : ' + PORT));
+// Start Server
+app.listen(PORT, () => console.log("Server is running at port: " + PORT));
